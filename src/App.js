7103029e160f388c6f986b391f09da24990f8ad5
AnasNajjar1 <anas.najjar@sesame.com.tn>
2021-05-06 19:51:06 +0200

    started signin section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Link, BrowserRouter, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
 
@@ -31,6 +32,7 @@ function App() {
         <main>
           <Route path="/cart/:id?" component={CartScreen} />
           <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/signin" component={SigninScreen} />
           <Route path="/" component={HomeScreen} exact />
           
         </main>
