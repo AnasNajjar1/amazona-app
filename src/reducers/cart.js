69b84b242f9eef52c095459796b216d1c75f6b68
AnasNajjar1 <anas.najjar@sesame.com.tn>
2021-06-07 00:44:54 +0200

    forced order one seller

--- src/reducers/cart.js
+++ src/reducers/cart.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_EMPTY, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_ADD_ITEM_FAILURE, CART_EMPTY, CART_REMOVE_ITEM, CART_SAVE_PAYMENT_METHOD, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstants";
 
 const initState = {
     cartItems: localStorage.getItem('cartItems')?
@@ -7,7 +7,8 @@ const initState = {
     shippingAddress: localStorage.getItem('shippingAddress')?
                      JSON.parse(localStorage.getItem('shippingAddress'))
                      : {},
-    paymentMethod: 'PayPal'
+    paymentMethod: 'PayPal',
+    error: null
 }
 
 export const cartReducer  = (state = initState, action) => {
@@ -18,22 +19,26 @@ export const cartReducer  = (state = initState, action) => {
             if(existItem) {
                 return {
                     ...state, 
+                    error: null,
                     cartItems: state.cartItems.map((x) => x.product === existItem.product? item : x)
                 }
             } else {
                 return {
                     ...state,
+                    error: null,
                     cartItems: [...state.cartItems, item]
                 }
             }
+        case CART_ADD_ITEM_FAILURE:
+            return {...state, error: action.payload}
         case CART_REMOVE_ITEM:
-            return {...state, cartItems: state.cartItems.filter(x => x.product !== action.payload)};
+            return {...state, error: null, cartItems: state.cartItems.filter(x => x.product !== action.payload)};
         case CART_SAVE_SHIPPING_ADDRESS:
             return {...state,  shippingAddress: action.payload};
         case CART_SAVE_PAYMENT_METHOD:
             return {...state, paymentMethod: action.payload};
         case CART_EMPTY:
-            return {...state, cartItems: []};
+            return {...state, error: null, cartItems: []};
         default: 
             return state;
     }
