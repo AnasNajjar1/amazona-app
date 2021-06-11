409ba1549a76b6472d14e90aedf3c66658611271
Anas Najjar <anas.najjar@sesame.com.tn>
2021-06-11 02:16:44 +0200

    almost finished seller part

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,9 +13,15 @@ const Product = ({ product }) => {
                     <h2>{product.name}</h2>
                 </Link>
                 <Rating rating={product.rating} numReviews={product.numReviews} />
-                <div className="price">
-                    ${product.price}
+                <div className="row">
+                    <div className="price">
+                        ${product.price}
+                    </div>
+                    <div>
+                        <Link to={`/seller/${product.seller._id}`}>{product.seller.seller.name}</Link>
+                    </div>
                 </div>
+                
             </div>
         </div>
     )
